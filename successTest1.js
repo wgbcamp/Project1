86a772d3ca332dd16082bc82727c0be6333b0913
Nadia <nadiamiswiyati@gmail.com>
2019-12-17 17:45:26 -0500

    updated

--- successTest1.js
+++
@@ -1,9 +0,0 @@
-document.write("hey this is testing . ");
-
-
-
-let happyEmoji = "🎈🎢🙂😘";
-for (let char of happyEmoji) {
-    //console.log(char);
-    document.write(happyEmoji, " Yeah it work!! ");       
-}
