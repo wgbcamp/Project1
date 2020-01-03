ec3f54b547d8f65ea37a045ccf1fc1ea433f6aad
Warren Brown <warrengbrn@gmail.com>
2020-01-03 07:08:39 -0500

    added restaurantFinder function to find restaurants in a specific radius based on Google's coordinates of address entered into input field

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,23 +14,14 @@
 
 
 //latitude and longitude values will be held here
-
-var longitude;
-var latitude;
+var longitudeValue;
+var latitudeValue;
 var placeSentToGeocode;
 var apikey = "YOUR_API_KEY";
 
 
-//hitting enter invokes geocode function
-document.addEventListener("keyup", function(event){
-    if(event.key == "Enter"){
-        geocode2();
-    }
-});
 
-
-//autocomplete function
-
+//**autocomplete function starts after user types text into search bar**
 google.maps.event.addDomListener(window, 'load', initialize);
 
 function initialize() {
@@ -46,8 +37,8 @@ var place = autocomplete.getPlace();
 console.log("Place changed for: " + place.formatted_address);
 console.log("Latitude: " + place.geometry['location'].lat());
 console.log("Longitude: " + place.geometry['location'].lng());
-latitude = place.geometry['location'].lat();
-longitude = place.geometry['location'].lng();
+latitudeValue = place.geometry['location'].lat();
+longitudeValue = place.geometry['location'].lng();
 placeSentToGeocode = place.formatted_address;
 
 if(placeSentToGeocode !== undefined){
@@ -59,37 +50,31 @@ if(placeSentToGeocode !== undefined){
 
 
 
-
-
-    
-
-
-
-
-//Geocode function is called after autocomplete function, requesting 
-
+//**Geocode function is called after autocomplete option is selected**
 function geocode(){
    
    console.log("Geocode function called...")
 
 
-    axios.get('https://maps.googleapis.com/maps/api/geocode/json?', {
-       params:{
-           address: placeSentToGeocode,
-           key: apikey
-       } 
-    })
+   $.ajax({
+       url: 'https://maps.googleapis.com/maps/api/geocode/json?',
+       data: {
+       address: placeSentToGeocode,
+       key: apikey
+    }
+   })
+
     .then(function(response){
         //log full response
         console.log(response);
 
         // Latitude and logitude coordinates 
-         latitude = (response.data.results[0].geometry.location.lat);
-         longitude = (response.data.results[0].geometry.location.lng);
+         latitudeValue = (response.results[0].geometry.location.lat);
+         longitudeValue = (response.results[0].geometry.location.lng);
         
 
        initMap(); 
-
+       restaurantFinder();
     })
     .catch(function(error){
         console.log(error)
@@ -98,30 +83,36 @@ function geocode(){
     
 }
 
-//hitting enter starts this geocode function
+//**Hitting enter invokes geocode function, searching for address that best matches user input**
+document.addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        geocode2();
+    }
+});
 
 function geocode2(){
    
     console.log("Geocode function called after ENTER press...")
  
     var location = document.getElementById('userInput').value;
-    axios.get('https://maps.googleapis.com/maps/api/geocode/json?', {
-        params:{
+    $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?',
+        data: {
             address: location,
             key: apikey
         } 
-     })
+    })
      .then(function(response){
          //log full response
          console.log(response);
  
          // Latitude and logitude coordinates 
-          latitude = (response.data.results[0].geometry.location.lat);
-          longitude = (response.data.results[0].geometry.location.lng);
+          latitudeValue = (response.results[0].geometry.location.lat);
+          longitudeValue = (response.results[0].geometry.location.lng);
          
  
         initMap(); 
- 
+        restaurantFinder();
      })
      .catch(function(error){
          console.log(error)
@@ -132,13 +123,7 @@ function geocode2(){
 
 
 
-
-
-
-
-
-
-//initialize map........................................
+//**map is initialized after geocode function retrieves location data**
 function initMap(){
 
 console.log("Map function called...")
@@ -146,22 +131,86 @@ console.log("Map function called...")
 //Map options
 var options = {
 zoom:18,
-center: {lat: latitude, lng: longitude}
+center: {lat: latitudeValue, lng: longitudeValue}
 }
 
 //New map
+document.getElementById('mapTitle').innerHTML = "Map";
 var map = new google.maps.Map(document.getElementById('map'), options);
 
 //Add marker
 var marker = new google.maps.Marker({
-position:{lat: latitude, lng: longitude},
+position:{lat: latitudeValue, lng: longitudeValue},
 map:map
-
-
 
 });
 
 console.log("Map and marker displayed.")
 }
+
+
+
+//**restaurantFinder finds the place ID of restaurants within 1 mile of user's coordinates**  
+function restaurantFinder(){
+
+    console.log("restaurantFinder function called...")
+
+    var stringifiedPosition = String(latitudeValue) + "," + String(longitudeValue);
+    console.log("Stringified is..." + stringifiedPosition);
+    $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
+
+        data: {
+            location: stringifiedPosition,
+            radius: '1609.34',
+            type: 'restaurant',
+            key: apikey
+        
+        }
+    })
+
+  
+
+    .then(function(response){
+        console.log(response);
+
+       
+        var restaurantsOutput = '<ul class="list-group">';
+
+        for (i=0; i < 12; i++){
+            var restaurants = response.results[i].name;
+           restaurantsOutput+= 
+                `<li class="list-group-item">${restaurants}</li>`
+           ;
+        }
+        
+        document.getElementById('restaurantTitle').innerHTML = "Restaurants";
+        document.getElementById('restaurantPoints').innerHTML = restaurantsOutput; 
+
+    })
+    .catch(function(error){
+        console.log(error)
+    });
+}
+
+
+
+// function restaurantData(){
+
+//     console.log("restaurantData function called...")
+
+//     $.ajax({
+//         url: '',
+
+//         data: {
+//             key: apikey,
+//             place_id: 
+
+
+
+//         }
+
+//     })
+// }
 
 
