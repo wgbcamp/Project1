62fbf00833425ee3c1ac31acffe6924091a42cd1
Nadia <nadiamiswiyati@gmail.com>
2020-01-03 22:25:15 -0500

    addedcode

---
+++ project1code/main.js
@@ -0,0 +1,29 @@
+//prompt('this is testing');
+
+/*
+var url = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=32e8183a7007303bba605abc3fd9efbe";
+
+getWeather()
+
+function getWeather() {
+    $.ajax({
+        type: "GET",
+        url: url,
+    
+    }).then(function (res) { console.log(res);
+     });
+} */
+
+
+
+    //THIS FOR THE WEATHER 
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+            
+
+
+
+
+    //THIS JQUERY FOR LIST
+    $('[href]').css('color', 'darkblue');
+
+    
