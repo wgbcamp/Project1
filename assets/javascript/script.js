67ba89f10109d0b3a20950b380284da50275d359
wgbcamp <56795202+wgbcamp@users.noreply.github.com>
2020-01-09 13:45:07 -0500

    Add files via upload
    Adding function to show hotels, edited function to clear page on new search, allow content to append to page instead of hooking into several permanent divs in html file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,18 +2,13 @@ var longitudeValue;
 var latitudeValue;
 var placeSentToGeocode;
 var apikey = "YOUR_API_KEY";
-var restaurants;
 var place_idVariable;
-var restaurantsOutput = '<ul style="list-style-type:none;">';
 var photo;
-var photoLoopCounter=0;
-var boxCounter = 1;
-
-
-
+var savedAppendRestaurants;
 var dayNumber = new Date();
 var dayOutput = dayNumber.getDay();
-
+var place_idVariableForLodging;
+var savedAppendLodging;
 
 //**autocomplete function starts after user types text into input field**
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -44,7 +39,7 @@ if(placeSentToGeocode !== undefined){
 
 
 
-//**Geocode function is called after autocomplete option is selected**
+//**Geocode function is called after autocomplete option is selected, clearing div of past results**
 function geocode(){
    
    console.log("Geocode function called...")
@@ -67,14 +62,17 @@ function geocode(){
         
 
        initMap(); 
+	   document.getElementById("restaurantPoints").innerHTML = "";
+	   document.getElementById("lodgingPoints").innerHTML = "";
        restaurantFinder();
+	   lodgingFinder();
     })
     .catch(function(error){
         console.log(error)
     }); 
 }
 
-//**Hitting enter invokes geocode function, searching for address that best matches user input**
+//**Hitting enter invokes geocode function, searching for address that best matches user input, clearing div of past results**
 document.addEventListener("keyup", function(event){
     if(event.key == "Enter"){
         geocode2();
@@ -103,7 +101,10 @@ function geocode2(){
          
  
         initMap(); 
+		document.getElementById("restaurantPoints").innerHTML = "";
+		document.getElementById("lodgingPoints").innerHTML = "";
         restaurantFinder();
+		lodgingFinder();
      })
      .catch(function(error){
          console.log(error)
@@ -143,9 +144,7 @@ console.log("Map and marker displayed.")
 
 //**restaurantFinder finds the place ID of restaurants within 1 mile of user's coordinates**  
 function restaurantFinder(){
-    //refresh the restaurantPoints div in html file .....commented!
-    restaurantsOutput = "";
-    // document.getElementById('restaurantPoints').innerHTML = restaurantsOutput;  commented!
+
     console.log("restaurantFinder function called...")
 
     var stringifiedPosition = String(latitudeValue) + "," + String(longitudeValue);
@@ -161,38 +160,25 @@ function restaurantFinder(){
         
         }
     })
-
-  
-
     .then(function(response){
         console.log(response);
 
-       
-        
-
         for (i=0; i < 10; i++){
-           
            place_idVariable = response.results[i].place_id;
-           
            restaurantData();
         } 
     })
     .catch(function(error){
-        console.log(error)
-        
-    });
-
-    
+        console.log(error)      
+    });   
 }
 
 
-//**restaurantData is called at the end of restaurantFinder, providing more info about each restaurant **/
+//**restaurantData is called at the end of restaurantFinder, providing more info about each restaurant and appending to div **/
 function restaurantData(){
 
     console.log("restaurantData function called...")
     
-
-
         $.ajax({
             url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?',
     
@@ -203,39 +189,153 @@ function restaurantData(){
         })
         .then(function(response){
             console.log(response);
-            restaurants = response.result.name;
-            photo = response.result.photos[photoLoopCounter].photo_reference; 
-                
-                document.getElementById('restaurantTitle').innerHTML = "Restaurants";
-                document.getElementById('shoppingTitle').innerHTML = "Shopping";
-//^^^^^ placeholder
+			
+			document.getElementById('restaurantTitle').innerHTML = "Restaurants";
+            document.getElementById('lodgingTitle').innerHTML = "Hotels";
+            
+				photo = response.result.photos[1].photo_reference; 
+				var storethis = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=' + photo + '&key=' + apikey;
+				var restNameV = response.result.name;
+				var restAddressV = response.result.formatted_address;
+				var restPhoneNumberV = response.result.formatted_phone_number;
+				var restHoursV = response.result.opening_hours.weekday_text[dayOutput];
+				
+				$('#restaurantPoints').append(`
+				
+				                    <div class="columns has-text-center">
+                        <div class="column is-6" id="photoSpot">
+                            <img id="thePhoto" src="${storethis}">
+                        </div>
+                        <div class="box">
 
-                document.getElementById('restName' + boxCounter).innerHTML = response.result.name;
-                document.getElementById('restAddress' + boxCounter).innerHTML = response.result.formatted_address;
-                document.getElementById('restPhoneNumber' + boxCounter).innerHTML = response.result.formatted_phone_number;
-                document.getElementById('restHours' + boxCounter).innerHTML = response.result.opening_hours.weekday_text[dayOutput];
+                        <div class="column is-6" id="infoSpot">
+                            <div class="columns boldThis" id="restName">
+                                ${restNameV}
+                            </div>
+                            <div class="columns" id="restAddress">
+                                ${restAddressV}
+                            </div>
+                            <div class="columns" id="restPhoneNumber">
+                                ${restPhoneNumberV}
+                            </div>
+                            <div class="columns" id="restHours">
+                                ${restHoursV}
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                    <br>
+                    <br>
+				
+				`);
+				
                 
-                
-                //loading page elements
+                //show page elements
                 document.getElementById('hiddencolumns').style.visibility = "visible";
+                //saving results for now
+				savedAppendRestaurants = document.getElementById("restaurantPoints").innerHTML;
                 
-                photoLoopCounter++;
-                if(photoLoopCounter== 10){
-                    photoLoopCounter -= 10;
-                }
-                boxCounter++;
-                if(boxCounter == 10){
-                    boxCounter -= 9;
-                }
-                restaurantPhoto();
         })
 }
 
-function restaurantPhoto(){
-    console.log("restaurantPhoto funcation called...")
-    var storethis = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=' + photo + '&key=' + apikey;
-    $("#thePhoto" + photoLoopCounter).attr("src",storethis);
-    console.log("photolooper is " + photoLoopCounter);
-    console.log("boxcounter is " + boxCounter);
+
+
+
+//**lodgingFinder finds the place ID of hotels within 1 mile of user's coordinates**  
+function lodgingFinder(){
+
+    console.log("restaurantFinder function called...")
+
+    var stringifiedPosition = String(latitudeValue) + "," + String(longitudeValue);
+    console.log("Stringified is..." + stringifiedPosition);
+    $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
+
+        data: {
+            location: stringifiedPosition,
+            radius: '1609.34',
+            type: 'lodging', 
+            key: apikey
+        
+        }
+    })
+    .then(function(response){
+        console.log(response);
+
+        for (i=0; i < 10; i++){
+           place_idVariableForLodging = response.results[i].place_id;
+           lodgingData();
+        } 
+    })
+    .catch(function(error){
+        console.log(error)      
+    });   
+}
+
+
+//**lodgingData is called at the end of lodgingFinder, providing more info about each hotel and appending to div **/
+function lodgingData(){
+
+    console.log("restaurantData function called...")
+    
+        $.ajax({
+            url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?',
+    
+            data: {
+                key: apikey,
+                place_id: place_idVariableForLodging,
+            }
+        })
+        .then(function(response){
+            console.log(response);
+			
+			document.getElementById('restaurantTitle').innerHTML = "Restaurants";
+            document.getElementById('lodgingTitle').innerHTML = "Hotels";
+            
+				photo = response.result.photos[1].photo_reference; 
+				var storethis = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=' + photo + '&key=' + apikey;
+				var lodgNameV = response.result.name;
+				var lodgAddressV = response.result.formatted_address;
+				var lodgPhoneNumberV = response.result.formatted_phone_number;
+ 				var lodgHoursV = response.result.opening_hours.weekday_text[dayOutput];
+ 				
+				$('#lodgingPoints').append(`
+				
+				                    <div class="columns has-text-center">
+                        <div class="column is-6" id="photoSpot">
+                            <img id="thePhoto" src="${storethis}">
+                        </div>
+                        <div class="box">
+
+                        <div class="column is-6" id="infoSpot">
+                            <div class="columns boldThis" id="restName">
+                                ${lodgNameV}
+                            </div>
+                            <div class="columns" id="restAddress">
+                                ${lodgAddressV}
+                            </div>
+                            <div class="columns" id="restPhoneNumber">
+                                ${lodgPhoneNumberV}
+                            </div>
+                            <div class="columns" id="restHours">
+                                ${lodgHoursV}
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                    <br>
+                    <br>
+				
+				`);
+				
+                
+                //loading page elements
+                document.getElementById('hiddencolumns').style.visibility = "visible";
+                //saving results for now
+				savedAppendLodging = document.getElementById("lodgingPoints").innerHTML;
+                
+        })
 }
 
